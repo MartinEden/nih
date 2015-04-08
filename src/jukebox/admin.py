53365a303f2199223037287f8f7425b884ef7f00
Tom Parker <palfrey@tevp.net>
2015-04-08 10:44:47 +0100

    Don't need the old admin site bits

--- src/jukebox/admin.py
+++
@@ -1,7 +0,0 @@
-from jukebox.models import *
-from django.contrib import admin
-
-admin.site.register(WebPath)
-admin.site.register(MusicFile)
-admin.site.register(QueueItem)
-admin.site.register(ChatItem)
