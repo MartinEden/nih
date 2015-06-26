7933e9ef45036c3926f0c6b5cf061152d1853e41
Tom Parker <palfrey@tevp.net>
2015-06-26 16:12:21 +0100

    Need more Gstreamer bits

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git submodule update --init --recursive
-sudo apt-get install python-pip python-musicbrainz2 python-alsaaudio python-mysqldb mysql-server python-gst0.10 gstreamer0.10-plugins-base python-virtualenv
+sudo apt-get install python-pip python-musicbrainz2 python-alsaaudio python-mysqldb mysql-server python-gst0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad python-virtualenv
 python scripts/generate-virtualenv-bootstrap.py
 
 if [ ! -d "ENV" ]; then
